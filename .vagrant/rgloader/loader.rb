4769ca725e78cfa543f809ef2782a0c4e1299e92
Jesse-Richard <jesse.mulwa@gmail.com>
2024-10-30 09:24:30 +0300

    config alignment

--- .vagrant/rgloader/loader.rb
+++ .vagrant/rgloader/loader.rb
@@ -1,5 +1,6 @@
 # This file loads the proper rgloader/loader.rb file that comes packaged
 # with Vagrant so that encoded files can properly run with Vagrant.
+# rgloader VBox aligned
 
 if ENV["VAGRANT_INSTALLER_EMBEDDED_DIR"]
   require File.expand_path(
